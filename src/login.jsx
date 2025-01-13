5f58a8822ede0f2fcc0d64f27b6dc2d46ca67e16
Bill Steinbock <bsteinbock@microtraks.com>
2025-01-13 14:52:46 -0500

    update the login process

--- src/login.jsx
+++ src/login.jsx
@@ -19,32 +19,46 @@ const Login = () => {
   // Access the API URL using import.meta.env
   const apiUrl = import.meta.env.VITE_API_URL;
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const response = await fetch(`${apiUrl}/api/v1/auth/user`, {
-          method: 'GET',
-          credentials: 'include',
-        });
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = await response.json();
-        const { user, token } = data;
-
-        login(user, token);
-      } catch (error) {
-        logout();
-      }
-    };
-
-    fetchUser();
-  }, []);
-
+  //  useEffect(() => {
+  //    const fetchUser = async () => {
+  //      try {
+  //        const response = await fetch(`${apiUrl}/api/v1/auth/user`, {
+  //          method: 'GET',
+  //          credentials: 'include',
+  //        });
+  //        if (!response.ok) {
+  //          throw new Error('Network response was not ok');
+  //        }
+  //        const data = await response.json();
+  //        const { user, token } = data;
+  //
+  //        login(user, token);
+  //      } catch (error) {
+  //        logout();
+  //      }
+  //    };
+  //
+  //    fetchUser();
+  //  }, []);
+  //
   const handleLogin = () => {
     // Redirect to backend's Google OAuth route
     window.location.href = `${apiUrl}/api/v1/auth/google`; //login route
   };
+
+  useEffect(() => {
+    // Check if the token is present in URL after redirection
+    const urlParams = new URLSearchParams(window.location.search);
+    const tokenFromURL = urlParams.get('token');
+    const emailFromURL = urlParams.get('email');
+    const fullNameFromURL = urlParams.get('fullName');
+    const userFromURL = { email: emailFromURL, fullName: fullNameFromURL };
+
+    if (tokenFromURL) {
+      login(userFromURL, tokenFromURL);
+      window.history.replaceState(null, '', window.location.pathname); // Remove token from URL
+    }
+  }, []);
 
   const handleLogout = async () => {
     try {
